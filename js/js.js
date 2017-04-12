b534658ca63a59937bff260c4ea426001cabe3c0
Azim <azimsadikov@gmail.com>
2017-04-12 17:39:22 -0500

    I made a gallery with navigation bar

---
+++ js/js.js
@@ -0,0 +1,20 @@
+var products = [];
+// Create an array to hold your products.
+var basil = {
+    description: "basil",
+    price: "price",
+    URL:
+};
+// Each item in the array must be an object that contains the following information: name, description, price, and URL for product image. You can add more properties if that's what the team wants, but it must have those.
+// The team will use JavaScript to add each product to the DOM.
+// Each product must be a card, much like everyone made in the Static Web quiz. As a team, decide on the style of the card.
+// The layout of the product page should be a grid of cards. 3-wide, 4-wide, 5-wide, the teams must decide how many cards are on each row
+
+
+var url = document.createElement('a');
+url.href = "http://www.example.com/some/path?name=value#anchor";
+var protocol = url.protocol;
+var hash = url.hash;
+
+console.log(hash);
+
